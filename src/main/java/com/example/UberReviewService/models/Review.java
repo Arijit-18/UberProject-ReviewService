ddb664420e0417ff63d1391242f0a60a0e713ec7
Arijit <arijitchandra45@gmail.com>
2024-08-17 13:18:01 +0530

    added more columns for review table

--- src/main/java/com/example/UberReviewService/models/Review.java
+++ src/main/java/com/example/UberReviewService/models/Review.java
@@ -1,18 +1,39 @@
 package com.example.UberReviewService.models;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
 
 //ORM like hibernate helps us to create tables in the database by writing class based java code.
 
 //this annotation helps java know that this class is the logical representation of our db table.
+
+//Table annotation is going to manipilate things on the database where as Entity annotation manipulates thing on the java end.
+
 @Entity
+@Table(name = "bookingreview")
 public class Review {
 
     //@Id annotation makes the id property a primary key of our table
     @Id
-//    @GeneratedValue(strategy = )
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //identity means auto increment
     Long Id;
+
+    @Column(nullable = false)
+    String reviewContent;
+
+    Double rating;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP) // this tells spring about the format of Date object to be stored i.e Date/Time/Timestamp
+    @CreatedDate //this annotation tells spring that only handle for object creation
+    Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate //this annotation tells spring that only handle for object updation
+    Date updatedAt;
 }
